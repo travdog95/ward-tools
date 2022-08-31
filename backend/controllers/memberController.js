8a96ea83ce59807647a17959a4ec1511b6c3d12f
Travis Peters <travis@tkopeters.com>
2022-08-31 16:43:34 -0600

    Initial commit for Members API

--- backend/controllers/memberController.js
+++ backend/controllers/memberController.js
@@ -1,42 +1,78 @@
 const asyncHandler = require("express-async-handler");
 
+const Member = require("../models/memberModel");
+
 // @desc    Get members
 // @router  GET /api/members
 // @access  Private
 const getMembers = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get members" });
+  const members = await Member.find();
+
+  res.status(200).json(members);
 });
 
 // @desc    Add member
 // @router  POST /api/members
 // @access  Private
 const addMember = asyncHandler(async (req, res) => {
-  if (!req.body.name) {
+  if (!req.body.text) {
     res.status(400);
     throw new Error("Please add a name field");
   }
-  res.status(200).json({ message: "Add member" });
+
+  const member = await Member.create({
+    text: req.body.text,
+  });
+
+  res.status(200).json(member);
 });
 
 // @desc    Update member
 // @router  PUT /api/members/:id
 // @access  Private
 const updateMember = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Update member" });
+  const member = await Member.findById(req.params.id);
+
+  if (!member) {
+    res.status(400);
+    throw new Error("Member not found");
+  }
+
+  const updatedMember = await Member.findByIdAndUpdate(req.params.id, req.body, { new: true });
+
+  res.status(200).json(updatedMember);
 });
 
 // @desc    Patch member
 // @router  PATCH /api/members/:id
 // @access  Private
 const patchMember = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Patch member" });
+  const member = await Member.findById(req.params.id);
+
+  if (!member) {
+    res.status(400);
+    throw new Error("Member not found");
+  }
+
+  const patchedMember = await Member.findByIdAndUpdate(req.params.id, req.body, { new: true });
+
+  res.status(200).json(patchedMember);
 });
 
 // @desc    Delete member
 // @router  DELETE /api/members/:id
 // @access  Private
 const deleteMember = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Delete member" });
+  const member = await Member.findById(req.params.id);
+
+  if (!member) {
+    res.status(400);
+    throw new Error("Member not found");
+  }
+
+  await member.remove();
+
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
