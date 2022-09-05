b8744e51eccdda4c4d1d869012326780289708dc
Travis Peters <travis@tkopeters.com>
2022-09-05 14:35:53 -0600

    Connected frontend and completed register and login

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,24 +1,23 @@
 import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
-// import { useSelector, useDispatch } from 'react-redux'
-// import { logout, reset } from '../features/auth/authSlice'
+import { useSelector, useDispatch } from "react-redux";
+import { logout, reset } from "../features/auth/authSlice";
 
 const Header = () => {
-  // const navigate = useNavigate()
-  // const dispatch = useDispatch()
-  // const { user } = useSelector((state) => state.auth)
-  const user = false;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
 
   const onLogout = () => {
-    // dispatch(logout())
-    // dispatch(reset())
-    // navigate('/')
+    dispatch(logout());
+    dispatch(reset());
+    navigate("/");
   };
 
   return (
     <header className="header">
       <div className="logo">
-        <Link to="/">GoalSetter</Link>
+        <Link to="/">Home</Link>
       </div>
       <ul>
         {user ? (
